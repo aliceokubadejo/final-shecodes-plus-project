9a65da9a10cdae1f00a2e6d04810606bbbbd47d2
aliceokubadejo <okubadejoalice@gmail.com>
2023-11-12 20:08:19 +0000

    forecast html instructions added to javascript

--- src/index.js
+++ src/index.js
@@ -33,20 +33,27 @@ function searchCity(event) {
   let apiKey = "3c949ba49d38be2487ee278e0d2d4059";
   let apiCityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityResult.value}&appid=${apiKey}&units=metric`;
   axios.get(apiCityUrl).then(showCurrentTemperature);
+  getForecast(response.data);
 }
 
 function showCurrentTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let tempheading = document.querySelector("h2");
+  let temperatureIconElement = response.data;
   let cityName = response.data.name;
   let heading = document.querySelector("h1");
-  let humidity = response.data.main.temp.humidity;
+  let humidity = response.data.main.humidity;
   let humidityElement = document.querySelector("#humidity");
+  let windSpeed = response.data.wind.speed;
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let conditions = response.data.weather[0].description;
+  let conditionsElement = document.querySelector("#conditions");
 
   heading.innerHTML = `${cityName}`;
   tempheading.innerHTML = `${temperature}°C`;
   humidityElement.innerHTML = `${humidity}`;
-  console.log(response);
+  windSpeedElement.innerHTML = `${windSpeed}`;
+  conditionsElement.innerHTML = `${conditions}`;
 }
 
 function showPositions(positions) {
@@ -55,7 +62,7 @@ function showPositions(positions) {
   let units = "metric";
   let apiKey = "3c949ba49d38be2487ee278e0d2d4059";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showCurrentTemperature);
+  axios(apiUrl).then(showCurrentTemperature);
 }
 
 function currentLocation(event) {
@@ -66,4 +73,42 @@ function currentLocation(event) {
 let currentLocationButton = document.querySelector("#current-button");
 currentLocationButton.addEventListener("click", currentLocation);
 
-let forecast = document.querySelector("#forecast");
+function getForecast(city) {
+  let apiKey = "cat07ba0af43c47b0ca92of160ade31e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+function displayForecast(response) {
+  console.log(response.data);
+  let forecastElement = document.querySelector("#forecast");
+  let forecastDays = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  forecastDays.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+           <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="weather-forecast-date">
+              ${day}
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/3222/3222791.png"
+                alt=""
+                width="36"
+              />
+            </div>
+            <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temperature-max">18</span>
+              <span class="weather-forecast-temperature-min">12</span>
+            </div>
+          </div>
+          </div>
+          </div>
+          </div>
+        `;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
+displayForecast();
